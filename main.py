26bdf2656060b6dd7a405df64e278f07ef12fca5
ravi-sidharth <thenewravy@gmail.com>
2024-10-05 18:38:13 +0530

    update

---
+++ main.py
@@ -0,0 +1,9 @@
+arr = list(map(int,input().split()))
+
+newArr=[]
+i=0
+while i<len(arr):
+    newArr+=[arr[i]]
+    i+=1
+    
+print(newArr)
---
+++ script.js
@@ -0,0 +1,75 @@
+let url = 'https://newsapi.org/v2/everything?';
+let apiKey = 'f640f1ff0a114321b25177eb7a462a99';
+
+
+window.addEventListener('load', ()=> {
+    getNews("india");
+})
+
+function reload() {
+    window.location.reload()
+}
+
+async function getNews(query) {
+    let res = await fetch(`${url}q=${query}&apiKey=${apiKey}`)
+    let data = await res.json()
+     bindData(data.articles)
+    // console.log(data.articles)
+}
+
+
+function bindData(articles) {
+    const cardsContainer = document.getElementById('card-container');
+    const newsCardTemplate = document.getElementById('news-card-template');
+
+    cardsContainer.innerHTML="";
+
+    articles.forEach((article) => {
+        if (!article.urlToImage) return
+        const cardClone = newsCardTemplate.content.cloneNode(true);
+        fillDataInCard(cardClone,article);
+        cardsContainer.appendChild(cardClone);
+    });
+}
+
+
+function fillDataInCard(cardClone,article) {
+    const newsImg = cardClone.querySelector('img');
+    const newsTitle = cardClone.querySelector('h3');
+    const newsSource = cardClone.querySelector('h6');
+    const newsDesc  = cardClone.querySelector('p');
+   
+    newsImg.src=article.urlToImage;
+    newsTitle.innerText=article.title;
+    newsDesc.innerText=article.description;
+    
+    const date = new Date(article.publishedAt).toLocaleString("en-US", {
+        timeZone:"Asia/Jakarta"
+    })
+    newsSource.innerText=`${article.source.name} - ${date}`
+
+    cardClone.firstElementChild.addEventListener('click',()=> {
+        window.open(article.url,"_blank")
+    })
+}
+
+currentActiveNav=null;
+function onNavItemClick(id) {
+    getNews(id)
+    const navItem =document.getElementById(id)
+    currentActiveNav?.classList.remove('active')
+    currentActiveNav=navItem
+    currentActiveNav.classList.add('active')
+}
+
+
+const newsInput = document.getElementById('news-input')
+const searchBtn = document.getElementById('search-button')
+
+searchBtn.addEventListener('click',()=> {
+    const query = newsInput.value;
+    getNews(query);
+    currentActiveNav?.classList.remove('active');
+    currentActiveNav=Null;
+
+})
